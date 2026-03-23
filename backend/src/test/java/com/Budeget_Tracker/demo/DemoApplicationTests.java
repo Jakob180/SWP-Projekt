e5813d610add7e5c784fff1597a7bc9751626aa8
jakob <jakob.fagerer.student@htl-hallein.at>
2026-03-23 10:21:09 +0100

    Sola

---
+++ backend/src/test/java/com/Budeget_Tracker/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Budeget_Tracker.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
